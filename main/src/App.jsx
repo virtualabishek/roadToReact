c2006c3796489f2f732adbc880a1355db3d97f61
virtualabishek <717abishekneupane@gmail.com>
2025-01-15 06:22:27 +0545

    learning: completed jan - 15,  day 4, morning first session

---
+++ main/src/App.jsx
@@ -0,0 +1,14 @@
+import AppMain from "./components/AppMain";
+
+const App = () => {
+  return (
+    <div>
+      {/* This AppMain contains all the orginal components and code from
+     road to react.*/}
+      <AppMain />
+      {/* The following components are some extra for exploring. */}
+    </div>
+  );
+};
+
+export default App;
---
+++ main/src/components/AppMain.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+const AppMain = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center">My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
+
+const Search = (props) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input
+        className="border-2 border-blue-600 bg-blue-300 text-pink-950"
+        id="search"
+        type="text"
+        onChange={props.onSearch}
+      />
+    </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+export default AppMain;
